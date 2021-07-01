51aa19a80cdd8e3bf293135eb4bb72abce399d2a
Al <alexseier12345@gmail..com>
2021-07-01 21:39:00 +0300

    lesson3

---
+++ lesson3/GB_804_758-ls3/src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <div v-html="dataHtml" />
+
+    <div :class="{ red: isRed }">
+      Error!!!
+    </div> -->
+
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" :date="computedDate"/> -->
+    <Calculator />
+
+    <hr />
+
+    <counter v-if="showCounterValue" @click="showCounter" ref="counter"/>
+    <button @click="showCounter">Change</button>
+    <!-- // вызов по поднятию клавиши(любой)
+    <input @keyup="onEnter">
+    
+    <button @click="onCkick">Click me </button> / {{ count }}
+    <button @click="count = 1 + 2">Sum</button>
+    <button @click="onCkick2(1, 4)">Click me2</button>
+    
+    {{ message }} -->
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+import Counter from './components/Counter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculator,
+    Counter
+  },
+  data(){
+    return {
+      showCounterValue: true,
+      // count: 0, 
+      // currentDate: null,
+      // message: "Hello Vue",
+      // dataHtml: "<div class='data'>12312</div>",
+      // isRed: false
+    }
+  },
+  methods: {
+    showCounter(){
+      this.$refs.counter.onClick()
+      console.log(this.$refs.counter.counter)
+    },
+    onCkick(){
+      this.count +=1
+    },
+    onCkick2(param, param2){
+      this.count = param + param2
+    },
+    onEnter(){
+      console.log('KeyUp')
+    }
+  },
+  computed: {
+    computedDate(){
+      return this.currentDate || "Текущая дата"
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.red{
+  color: red;
+}
+.data{
+  font-size: 20px;
+}
+</style>
+<style>
+.data{
+  font-size: 20px;
+}
+</style>
---
+++ lesson3/ls3mycost/GB_804_758-ls3mycost/src/components/PaymentsDisplay.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="list">
+        <table>
+            <thead>
+                <td>#</td>
+            </thead>
+        </table>
+        <div>idx</div><div>date</div><div>category</div><div>value</div>
+        <div class="item" 
+            v-for="(item, idx) in items" 
+            :key="idx">
+            {{ idx + 1 }} - {{ item }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PaymentsDisplay",
+    props: {
+        items: {
+            type: Array,
+            default: ()=>[],
+        }
+    }
+}
+</script>
