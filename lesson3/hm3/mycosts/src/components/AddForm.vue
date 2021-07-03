21d85da8af2c64007124026b6164728bf0f50a8e
Al <alexseier12345@gmail..com>
2021-07-03 13:06:22 +0300

    processing data done

--- lesson3/hm3/mycosts/src/components/AddForm.vue
+++ lesson3/hm3/mycosts/src/components/AddForm.vue
@@ -4,7 +4,7 @@
       <input type="text" placeholder="Payment description" v-model="category">
       <input type="number" placeholder="Payment amount" v-model="value">
       <input type="date" placeholder="Payment date" v-model="date">
-      <button @click="addPayment">ADD <span class="plus">+</span></button>
+      <button type="button" @click="addPayment">ADD <span class="plus">+</span></button>
     </form>
   </div>
 </template>
@@ -20,8 +20,27 @@ export default {
     }
   },
   methods: {
+    formatDate(date) {
+      let day = date.getDate();
+      if(day < 10) day = "0" + day
+      let month = date.getMonth() + 1;
+      if(month < 10) month = "0" + month
+      const year = date.getFullYear();
+
+      return `${day}.${month}.${year}`
+    },
+    currentDate(){
+      const date =  new Date();
+      return this.formatDate(date)
+    },
     addPayment() {
-      
+      const {category, value, date} = this;
+      const data = {
+        date: date ? this.formatDate(new Date(date)) : this.currentDate(),
+        category,
+        value
+      }
+      console.log(data);
     }
   }
 }
